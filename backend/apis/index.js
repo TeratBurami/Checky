000e3ffead6641c92a50b67297fcad0484d55383
Pathompum Jirakarnpaisan <107536914+Saannddy@users.noreply.github.com>
2025-10-21 10:53:15 +0700

    assignment and rubric update

--- backend/apis/index.js
+++ backend/apis/index.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import usersApi from "./user.js";
 import classesApi from "./class.js";
+import rubricApi from "./rubric.js";
+import assignmentApi from "./assignment.js";
 
 const router = Router();
 
 router.use("/auth", usersApi);
 router.use("/class", classesApi);
+router.use("/rubric", rubricApi);
+router.use("/class", assignmentApi)
 
 export default router;
