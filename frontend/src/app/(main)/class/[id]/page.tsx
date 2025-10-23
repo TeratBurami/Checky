b027696cdd6439562ea193cb6f5b8fea0e0d9699
TeratBurami <107537082+TeratBurami@users.noreply.github.com>
2025-10-23 22:50:13 +0700

    Class page 90% done, Completed full access token Login and register

--- frontend/src/app/(main)/class/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ClassDetail(){
-    return(
-        <div>Class Detail Page</div>
-    )
-}
--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import Navbar from "@/components/navbar";
-import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+import {JwtPayload} from '@/lib/types';
 
 export default function MainLayout({
   children,
@@ -12,14 +14,25 @@ export default function MainLayout({
   const [role, setRole] = useState<string | undefined>();
 
   useEffect(() => {
-    const userRole = Cookies.get("role");
-    setRole(userRole);
+    const token = Cookies.get("token");
+
+    if (token) {
+      try {
+        const decodedPayload = jwtDecode<JwtPayload>(token);
+        
+        console.log("Decoded role:", decodedPayload.role);
+        setRole(decodedPayload.role);
+
+      } catch (error) {
+        console.error("Failed to decode JWT:", error);
+      }
+    }
   }, []);
 
   return (
     <div>
         <Navbar role={role} />
-        <div className="mt-20">{children}</div>
+        <div className="mt-20 px-18 py-10">{children}</div>
     </div>
   );
 }
