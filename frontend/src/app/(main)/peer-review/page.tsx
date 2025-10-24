1ee0573399839da20109a2090abed75741aee335
JuiPi <sorawit2800@gmail.com>
2025-10-24 23:15:43 +0700

    merge

--- frontend/src/app/(main)/peer-review/page.tsx
+++ frontend/src/app/(main)/peer-review/page.tsx
@@ -1,5 +1,12 @@
 export default function PeerReview() {
     return(
-        <div>Peer Review Page</div>
+        <div className="mx-auto w-9/12">
+            <div className="flex flex-row justify-between items-center pt-[24px]">
+                <div>
+                    <h1 className="text-4xl font-bold">Peer Reviews</h1>
+                    <p className="text-sm font-medium text-neutral-500">Complete reviews for your peers to help them improve their writing.</p>
+                </div>
+            </div>
+        </div>
     )
 }
